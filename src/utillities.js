eb7eabf14cf11b52a80ef273facd57c698fa5290
NedLandy <matheus.v4200@gmail.com>
2020-09-09 17:42:26 -0300

    classe para utilitários do programa como textos e cores

---
+++ src/utillities.js
@@ -0,0 +1,50 @@
+module.exports = class Utillities {
+	static texts = {
+		pt: {
+			buttons:{
+				1: "Dashboards",
+				2: "Novo Dashboard",
+				3: "Começar Leitura",
+				4: "Entradas",
+				5: "Novo bloco",
+				6: "Deletar",
+				7: "Salvar",
+				8: "Sobre nós",
+				9: "Configurações"
+			}
+		},
+		en: {
+			buttons:{
+				1: "Dashboards",
+				2: "New Dashboard",
+				3: "Start Reading",
+				4: "Inputs",
+				5: "Add new Block",
+				6: "Delete",
+				7: "Save",
+				8: "About us",
+				9: "Config"
+			}
+		}
+	};
+
+	static colors = {
+		main: {
+			1: '#151620',
+			2: '#1d1e2f',
+			3: '#151620'
+		}
+	}
+
+	static theme = 'main'
+
+	static language = 'en'
+
+	static getColor(colorNumber) {
+		return (this.colors[this.theme])[colorNumber];
+	}
+
+	static getButtonText(buttonNumber) {
+		return (this.texts[this.language]).buttons[buttonNumber];
+	}
+};
