e35ed592bf0ec1109df261ebdd76d4bd3f3fd0b7
NedLandy <matheus.v4200@gmail.com>
2020-09-10 01:01:17 -0300

    importando menu de dashboards

--- src/components/sideMenu/menus.js
+++ src/components/sideMenu/menus.js
@@ -1,5 +1,5 @@
 const NewDashBoardMenu = require('./menus/newDashboard/newDashboard.js');
+const DashBoardsMenu = require('./menus/dashboards/dashBoards');
 
 
-
-module.exports = [NewDashBoardMenu];
+module.exports = [NewDashBoardMenu,DashBoardsMenu];
