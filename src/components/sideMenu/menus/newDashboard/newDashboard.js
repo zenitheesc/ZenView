434eba36803cf31cf2dd0a347f6586ffd372d8c9
NedLandy <matheus.v4200@gmail.com>
2020-09-11 21:13:50 -0300

    Menu funcional de criação de dashboard

--- src/components/sideMenu/menus/newDashboard/newDashboard.js
+++ src/components/sideMenu/menus/newDashboard/newDashboard.js
@@ -1,39 +1,65 @@
 const Menu = require('../menu');
 const Components = require('../../../components.js');
+const Validator = require('../../../../validator');
+const fs = require('fs');
+const DashBoard = require('../../../../classes/dashBoard');
 module.exports = class newDashBoardMenu extends Menu {
 	constructor() {
 		super('New Dashboard', 'newDashboard_menu');
-		this.createButton;
-		this.form;
-		this.logicComponent;
+		this.createButton = document.createElement('button');
+		this.form = Components.form();
+		this.logicComponent = new newDashBoardMenuLogic(this.createButton, this.form);
 	}
 	LoadAddNewDashBoardButton() {
-		this.createButton = document.createElement('button');
 		this.createButton.style.width = '50%';
 		this.createButton.style.marginLeft = '25%';
 		this.createButton.style.marginRight = '25%';
 		this.createButton.className = 'green-btn';
 		this.createButton.textContent = 'Criar';
-		this.createButton.type = 'button'; 
+		this.createButton.type = 'button';
 	}
 	newDashboardSpliter() {
-		this.form = document.createElement('form');
 
-		let dashBoardNameInput = Components.textInput('Nome', 'newDashBoardNameInput');
-		let dashBoardPath = Components.pathInput('Diretório', 'newDashsBoardPath');
-		let dashBoardNbmrOfInputs = Components.numberInput('Número de entradas', 'newDashBoardNbmrOfInputs', 1, 30);
-		let dashBoardDescription = Components.textArea('Descrição', 'newDashBoardDescription');
-		dashBoardDescription.childNodes[1].style.height = '16em';
+		let dashBoardNameInput = Components.textInput({
+			text: 'Nome',
+			id: 'newName',
+			tests: [Validator.isFilled, Validator.noSpecialChars]
+		});
+
+		let dashBoardPath = Components.pathInput({
+			text: 'Diretório',
+			id: 'newPath',
+			tests: [Validator.isFilled]
+		});
+
+		const isInRange = (value) => {
+			if (value <= 30 && value >= 1) {
+				return true;
+			} else {
+				return 'Deve ser um número entre 1 e 30';
+			}
+		};
+
+		let dashBoardNbmrOfInputs = Components.numberInput({
+			text: 'Número de entradas',
+			id: 'newNmbr',
+			tests: [Validator.isFilled, isInRange]
+		});
+		let dashBoardDescription = Components.textArea({
+			text: 'Descrição',
+			id: 'newDesc'
+		});
+		dashBoardDescription.htmlComponent.childNodes[1].style.height = '16em';
 
 		this.LoadAddNewDashBoardButton();
 
-		this.form.appendChild(dashBoardNameInput);
-		this.form.appendChild(dashBoardPath);
-		this.form.appendChild(dashBoardNbmrOfInputs);
-		this.form.appendChild(dashBoardDescription);
-		this.form.appendChild(this.createButton);
+		this.form.addField(dashBoardNameInput);
+		this.form.addField(dashBoardPath);
+		this.form.addField(dashBoardNbmrOfInputs);
+		this.form.addField(dashBoardDescription);
+		this.form.addComponent(this.createButton);
 
-		let spliter = Components.spliter('newDashboard', 'Criar novo Dashboard', this.form, true);
+		let spliter = Components.spliter('newDashboard', 'Criar novo Dashboard', this.form.htmlComponent, true);
 
 		return spliter;
 	}
@@ -52,56 +78,29 @@ module.exports = class newDashBoardMenu extends Menu {
 		spliterContainer.appendChild(this.importDashBoardSpliter());
 
 		this.menuComponent.appendChild(spliterContainer);
-		this.logicComponent = new newDashBoardMenuLogic(this.createButton,this.form);
 	}
 };
 
-class newDashBoardMenuLogic{
-	constructor(createDashBoardButton,createDashBoardForm){
+class newDashBoardMenuLogic {
+	constructor(createDashBoardButton, createDashBoardForm) {
 		this.createButton = createDashBoardButton;
 		this.form = createDashBoardForm;
-		this.createButton.addEventListener('click', () => this.getData(this.form.elements));
+		this.createButton.addEventListener('click', () => this.getData());
 	}
-	getData(data){
-		console.log(data);
-		const name = this.validateName(data[0].value);
-		const path = this.validateName(data[1].value);
-		const nbmrInput = this.validateName(data[2].value);
-		const description = this.validateName(data[4].value);
-		let validName = this.validateName(name);
-		let validePath = this.validatePath(path);
-		let validInputNbmr = this.validateInput(nbmrInput);
-		let validDescription = this.validateDesc(description);
-		if(validName === true && validePath === true &&  validInputNbmr === true && validDescription === true){
+	getData() {
+		if (this.form.validate()) {
+			let data = this.form.getData();
+			let i = 0;
+			let fileNames = fs.readdirSync(data['newPath']);
+			let newName = data['newName'] + '.json';
+			
+			while (fileNames.includes(newName)) {
+				i = i + 1;
+				newName = data['newName'] + `(${i})` + '.json';
+			}
 
+			let dashBoard = new DashBoard(data['newName'], data['newPath'], +'/' + newName, data['newNbmr'], data['newDesc']);
+			fs.writeFileSync(data['newPath'] + '/' + newName, JSON.stringify(dashBoard, null, '\t'));
 		}
-		return;
-	}
-	isEmpty(value){
-		return (value === '' || value === undefined)? true : false;
-	}
-	contarinSpecialChars(value){
-		const format = /[ `!@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?~]/;
-		return format.test(value);
-	}
-	validateName(name){
-		if(this.isEmpty(name)){
-			this.displayErrorMsg();
-		}
-	}
-	validatePath(path){
-
-	}
-	validateInput(inputNbmr){
-
-	}
-	validateDesc(description){
-		return true;
-	}
-	displayErrorMsg(id,message){
-
-	}
-	hideErrorMsg(id){
-
 	}
 }
