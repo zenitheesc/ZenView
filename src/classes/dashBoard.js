223af5a30b16fb491044dcea4087510b2ff868a2
NedLandy <matheus.v4200@gmail.com>
2020-09-13 01:17:45 -0300

    Criando sobrecarga de construtor sintética

--- src/classes/dashBoard.js
+++ src/classes/dashBoard.js
@@ -1,10 +1,31 @@
 module.exports = class DashBoard{
 	constructor(name,nbmrInputs,path,description){
+		this.name;
+		this.nbmrInputs;
+		this.inputs;
+		this.blocks;
+		this.path;
+		this.description;
+		if(arguments.length === 1){
+			this.constructFromJson();
+		}else{
+			this.newConstructor(name,nbmrInputs,path,description);
+		}
+	}
+	newConstructor(name,nbmrInputs,path,description){
 		this.name = name;
 		this.nbmrInputs = nbmrInputs;
 		this.inputs = {};
 		this.blocks = {};
 		this.path = path;
 		this.description = description;
+	}
+	constructFromJson(dashBoardJson){
+		this.name = dashBoardJson;
+		this.nbmrInputs = dashBoardJson;
+		this.inputs = dashBoardJson;
+		this.blocks = dashBoardJson;
+		this.path = dashBoardJson;
+		this.description = dashBoardJson;
 	}
 };
