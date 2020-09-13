04de98405e108576f0e52eb21adb38118858165d
NedLandy <matheus.v4200@gmail.com>
2020-09-13 18:48:26 -0300

    adicionando evento de atualização da lista de inputs

--- src/components/sideMenu/menus/inputs/inputs.js
+++ src/components/sideMenu/menus/inputs/inputs.js
@@ -3,20 +3,25 @@ const Components = require('../../../components.js');
 module.exports = class InputsMenu extends Menu {
 	constructor() {
 		super('Entradas', 'inputs_menu');
+		this.container = document.createElement('div');
+	}
+	attInputList() {
+		this.container.innerHTML = '';
+		let inputs = window['ZenViewConfig'].lastDasboard.inputs;
+		console.log(inputs);
+		inputs.forEach(input => {
+			this.container.appendChild(Components.inputCard(input.name, input.expression));
+		});
 	}
 	dashboardListSpliter() {
-		let container = document.createElement('div');
-		let savedDashboards = window['ZenViewConfig'].dashboards;
-		savedDashboards.forEach(dashboard => {
-			container.appendChild(Components.inputCard(dashboard.name, dashboard.desc, __dirname));
-			container.appendChild(Components.inputCard(dashboard.name, dashboard.desc, __dirname));
-		});
-		
-		let spliter = Components.spliter('dashBoardsList', 'Lista de inputs', container, true);
+		let spliter = Components.spliter('dashBoardsList', 'Lista de inputs', this.container, true);
 		spliter.style.height = '100%';
 		this.menuComponent.appendChild(spliter);
 	}
 	load() {
 		this.dashboardListSpliter();
+		window.addEventListener('attInputList',()=>{
+			this.attInputList();
+		});
 	}
 };
