0d8dbfd054c3e4dc81c602ce06f08596650a8337
NedLandy <matheus.v4200@gmail.com>
2020-09-13 01:16:41 -0300

    Adicionando a criação de dashboard

--- src/components/dashBoard/dashBoardComponent.js
+++ src/components/dashBoard/dashBoardComponent.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-
+const DashBoard = require('../../classes/dashBoard');
 module.exports = class DashBoardComponent {
-	changeGlobalContext(newContext){
+	changeGlobalContext(newContext) {
 		window.dispatchEvent(new CustomEvent('GlobalContextChange', {
 			detail: {
 				context: newContext,
@@ -11,13 +11,33 @@ module.exports = class DashBoardComponent {
 	editingDashBoard(details) {
 		console.log('iniciando dashboard para edição');
 		let DashBoard = fs.readFileSync(details.dashBoardPath);
-		window['ZenViewConfig'].lastDasboard = 
+		window['ZenViewConfig'].lastDasboard = DashBoard;
 		this.changeGlobalContext('editing');
 	}
 	startDashboard(details) {
 		console.log('iniciando dashboard para leitura');
 		let DashBoard = fs.readFileSync(details.dashBoardPath);
+		window['ZenViewConfig'].lastDasboard = DashBoard;
 		this.changeGlobalContext('start');
+	}
+	newDashBoard(detail) {
+		window['ZenViewConfig'].dashboards.unshift({
+			'name': detail.name,
+			'path': detail.path,
+			'desc': detail.desc
+		});
+
+		fs.writeFileSync('./src/config.json', JSON.stringify(window['ZenViewConfig'], null, '\t'));
+
+		let dashBoard = new DashBoard(detail.name, detail.numberOfInputs, detail.path, detail.desc);
+
+		fs.writeFileSync(detail.path, JSON.stringify(dashBoard, null, '\t'));
+		window.dispatchEvent(new CustomEvent('openDashBoard', {
+			detail: {
+				context: 'editing',
+				dashBoardPath: detail.path
+			}
+		}));
 	}
 	build() {
 		window.addEventListener('openDashBoard', (evt) => {
@@ -27,5 +47,9 @@ module.exports = class DashBoardComponent {
 				this.startDashboard(evt.detail);
 			}
 		});
+
+		window.addEventListener('newDashBoard', (evt) => {
+			this.newDashBoard(evt.detail);
+		});
 	}
-}
+};
