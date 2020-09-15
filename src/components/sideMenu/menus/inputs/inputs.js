5ac974d08cc056cb5612d56a7ddbc35f64341ac7
NedLandy <matheus.v4200@gmail.com>
2020-09-14 23:58:24 -0300

    Ínicio do novo menu de inputs

--- src/components/sideMenu/menus/inputs/inputs.js
+++ src/components/sideMenu/menus/inputs/inputs.js
@@ -1,26 +1,100 @@
 const Menu = require('../menu');
 const Components = require('../../../components.js');
+const Validator = require('../../../../validator');
+const Tribute = require('tributejs');
 module.exports = class InputsMenu extends Menu {
 	constructor() {
 		super('Entradas', 'inputs_menu');
-		this.container = document.createElement('div');
+		this.customForm = Components.form();
+		this.customDropDown;
 	}
 	attInputList() {
-		this.container.innerHTML = '';
-		let inputs = window['ZenViewConfig'].lastDasboard.inputs;
-		console.log(inputs);
-		inputs.forEach(input => {
-			this.container.appendChild(Components.inputCard(input.name, input.expression));
+		window['ZenViewConfig'].currentDashBoard.inputs.forEach(input => {
+			this.customDropDown.addOption(input);
 		});
 	}
-	dashboardListSpliter() {
-		let spliter = Components.spliter('dashBoardsList', 'Lista de inputs', this.container, true);
-		spliter.style.height = '100%';
-		this.menuComponent.appendChild(spliter);
+	customInputs() {
+		this.customDropDown = Components.dropDown({
+			text: 'Selecione uma entrada',
+			defaultText: 'Nova entrada',
+			defaultValue: 'newInput',
+			tests: [Validator.isFilled, Validator.noSpecialChars]
+		});
+		let inputName = Components.textInput({
+			text: 'Tag',
+			id: 'newName',
+			tests: [Validator.isFilled, Validator.noSpecialChars]
+		});
+
+		let inputExpression = Components.textArea({
+			text: 'Expressão',
+			id: 'newDesc'
+		});
+
+		let autoComplete = new Tribute({
+			noMatchTemplate: function () {
+				return '<span style:"visibility: hidden;"></span>';
+			},
+			collection: [{
+				trigger: '@',
+				values: [{
+					key: 'Phil Heartman',
+					value: 'pheartman'
+				},
+				{
+					key: 'Gordon Ramsey',
+					value: 'gramsey'
+				},
+
+				],
+				selectTemplate: function (item) {
+					return (
+						'<a contenteditable="false" class="inputTag">' +
+							'@' + item.original.value +
+							'</a>'
+					);
+
+				}
+			},
+			{
+				trigger: '#',
+				values: [{
+					key: 'Phil Heartman',
+					value: 'pheartman'
+				},
+				{
+					key: 'Gordon Ramsey',
+					value: 'gramsey'
+				}
+				],
+				selectTemplate: function (item) {
+					return (
+						'<a contenteditable="false" class="inputTag">' +
+							'#' + item.original.value +
+							'</a>'
+					);
+
+				}
+			}
+			]
+		});
+
+		autoComplete.attach(inputExpression.input);
+
+		this.customForm.addField(this.customDropDown);
+		this.customForm.addField(inputName);
+		this.customForm.addField(inputExpression);
+
+		let spliter = Components.spliter('customInputds', 'Entradas customizáveis', this.customForm.htmlComponent, true);
+
+		return spliter;
 	}
 	load() {
-		this.dashboardListSpliter();
-		window.addEventListener('attInputList',()=>{
+		let spliterContainer = document.createElement('div');
+		spliterContainer.className = 'menuBody';
+		spliterContainer.appendChild(this.customInputs());
+		this.menuComponent.appendChild(spliterContainer);
+		window.addEventListener('attInputList', () => {
 			this.attInputList();
 		});
 	}
