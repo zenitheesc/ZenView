a146b68e352e6e29e8e34fb2655c4b75fc85a04f
NedLandy <matheus.v4200@gmail.com>
2020-09-18 13:00:17 -0300

    novo menu de criação de dashboard com novo formBuilder

--- src/components/sideMenu/menus/newDashboard/newDashboard.js
+++ src/components/sideMenu/menus/newDashboard/newDashboard.js
@@ -2,58 +2,45 @@ const Menu = require('../menu');
 const Components = require('../../../components.js');
 const Validator = require('../../../../validator');
 const fs = require('fs');
+const Form = require('../../../../formBuilder').Form;
+const Container = require('../../../../formBuilder').Container;
+const Field = require('../../../../formBuilder').Field;
 module.exports = class newDashBoardMenu extends Menu {
 	constructor() {
 		super('New Dashboard', 'newDashboard_menu');
-		this.createButton = document.createElement('button');
-		this.form = Components.form();
-		this.logicComponent = new newDashBoardMenuLogic(this.createButton, this.form);
-	}
-	LoadAddNewDashBoardButton() {
-		this.createButton.style.width = '50%';
-		this.createButton.style.marginLeft = '25%';
-		this.createButton.style.marginRight = '25%';
-		this.createButton.className = 'green-btn';
-		this.createButton.textContent = 'Criar';
-		this.createButton.type = 'button';
-	}
-	newDashboardSpliter() {
-
-		let dashBoardNameInput = Components.textInput({
-			text: 'Nome',
-			id: 'newName',
-			tests: [Validator.isFilled, Validator.noSpecialChars]
+		this.button = Field.button({
+			text: 'Criar',
+			classList: ['formCenteredBtn', 'green-btn']
 		});
-
-		let dashBoardPath = Components.pathInput({
-			text: 'Diretório',
-			id: 'newPath',
-			tests: [Validator.isFilled]
+		this.form = new Form({
+			newDashBoardSpliter: Container.spliter({
+				Name: Field.text({
+					label: 'Nome',
+					att: 'name',
+					validators: [Validator.isFilled]
+				}),
+				Directory: Field.directory({
+					label: 'Diretório',
+					att: 'dir',
+					validators: [Validator.isFilled]
+				}),
+				NumberOfInputs: Field.number({
+					label: 'Número de entradas',
+					att: 'nOfInputs',
+					validators: [Validator.isFilled, Validator.isInRange(1, 30)]
+				}),
+				Description: Field.textArea({
+					label: 'Descrição',
+					att: 'desc',
+				}),
+				Save: this.button
+			}, {
+				startOpen: true,
+				text: 'Novo DashBoard',
+				id: 'newDashBoardSpliter'
+			})
 		});
-
-		let dashBoardNbmrOfInputs = Components.numberInput({
-			text: 'Número de entradas',
-			id: 'newNmbr',
-			tests: [Validator.isFilled,(x)=>{ return Validator.isInRange(x,1,30);} ]
-		});
-		let dashBoardDescription = Components.textArea({
-			text: 'Descrição',
-			id: 'newDesc'
-		});
-		
-		dashBoardDescription.htmlComponent.childNodes[1].style.height = '16em';
-
-		this.LoadAddNewDashBoardButton();
-
-		this.form.addField(dashBoardNameInput);
-		this.form.addField(dashBoardPath);
-		this.form.addField(dashBoardNbmrOfInputs);
-		this.form.addField(dashBoardDescription);
-		this.form.addComponent(this.createButton);
-
-		let spliter = Components.spliter('newDashboard', 'Criar novo Dashboard', this.form.htmlComponent, true);
-
-		return spliter;
+		console.log(this.form);
 	}
 	importDashBoardSpliter() {
 		let container = document.createElement('div');
@@ -62,45 +49,42 @@ module.exports = class newDashBoardMenu extends Menu {
 
 		return spliter;
 	}
+	setFormConfigs() {
+		this.button.onclick = () => {
+			if (this.form.validate()) {
+
+				let data = this.form.getData().form;
+				let i = 0;
+				console.log(data);
+				let fileNames = fs.readdirSync(data['dir']);
+				let newName = data['name'] + '.json';
+
+				while (fileNames.includes(newName)) {
+					i = i + 1;
+					newName = data['name'] + `(${i})` + '.json';
+				}
+				
+				window.dispatchEvent(new CustomEvent('newDashBoard', {
+					detail: {
+						name: data['name'],
+						numberOfInputs: data['nOfInputs'],
+						path: data['dir'] + '/' + newName,
+						desc: data['desc']
+					}
+				}));
+
+				this.form.clear();
+			}
+		};
+	}
 	load() {
 		let spliterContainer = document.createElement('div');
 		spliterContainer.className = 'menuBody';
 
-		spliterContainer.appendChild(this.newDashboardSpliter());
+		spliterContainer.appendChild(this.form.htmlComponent);
 		spliterContainer.appendChild(this.importDashBoardSpliter());
 
-		this.menuComponent.appendChild(spliterContainer);
+		this.menuComponent.appendChild(this.form.htmlComponent);
+		this.setFormConfigs();
 	}
 };
-
-class newDashBoardMenuLogic {
-	constructor(createDashBoardButton, createDashBoardForm) {
-		this.createButton = createDashBoardButton;
-		this.form = createDashBoardForm;
-		this.createButton.addEventListener('click', () => this.getData());
-	}
-	getData() {
-		if (this.form.validate()) {
-			let data = this.form.getData();
-			let i = 0;
-			let fileNames = fs.readdirSync(data['newPath']);
-			let newName = data['newName'] + '.json';
-
-			while (fileNames.includes(newName)) {
-				i = i + 1;
-				newName = data['newName'] + `(${i})` + '.json';
-			}
-
-			window.dispatchEvent(new CustomEvent('newDashBoard', {
-				detail:{
-					name: data['newName'],
-					numberOfInputs: data['newNmbr'],
-					path: data['newPath'] +'/' + newName,
-					desc: data['newDesc']
-				}
-			}));
-
-			this.form.clear();
-		}
-	}
-}
