2df0a73ab072925e80e1caad41b019c8363f8640
NedLandy <matheus.v4200@gmail.com>
2020-09-18 21:22:52 -0300

    delegandor responsabilidades para o input group e corrigindo erros

--- src/classes/dashBoard.js
+++ src/classes/dashBoard.js
@@ -2,32 +2,28 @@ const InputGroup = require('./InputGroup');
 module.exports = class DashBoard{
 	constructor(name,nbmrInputs,path,description){
 		this.name;
-		this.inputs =[];
+		this.inputGroup;
 		this.blocks;
 		this.path;
-		this._associationInput = {};
 		this.description;
 		if(arguments.length === 1){
-			this.constructFromJson();
+			this.constructFromJson(name);
 		}else{
 			this.newConstructor(name,nbmrInputs,path,description);
 		}
-	}
-	getInput(inputName){
-		return this._associationInput[inputName];
 	}
 	newConstructor(name,nbmrInputs,path,description){
 		this.name = name;
 		this.blocks = {};
 		this.path = path;
 		this.description = description;
-		this.inputs = new InputGroup(nbmrInputs);
+		this.inputGroup = new InputGroup(nbmrInputs);
 	}
 	constructFromJson(dashBoardJson){
-		this.name = dashBoardJson;
-		this.inputs = dashBoardJson;
-		this.blocks = dashBoardJson;
-		this.path = dashBoardJson;
-		this.description = dashBoardJson;
+		this.name = dashBoardJson.name;
+		this.inputGroup = dashBoardJson.inputGroup;
+		this.blocks = dashBoardJson.blocks;
+		this.path = dashBoardJson.path;
+		this.description = dashBoardJson.description;
 	}
 };
