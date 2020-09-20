09888aac83ffd7bb750aab0e0ccb4a0248ad3514
NedLandy <matheus.v4200@gmail.com>
2020-09-20 01:49:10 -0300

    novos métodos para a classe Input

--- src/classes/input.js
+++ src/classes/input.js
@@ -1,6 +1,24 @@
+const Math = require('mathjs');
 module.exports = class Input{
 	constructor(name,expression){
 		this.name = name;
 		this.expression = expression;
-	}	
+		this.dependencies;
+		this.expressionTree;
+	}
+	setDependencies(){
+		this.dependencies = this.expressionTree.filter((node)=>{
+			return node.isSymbolNode;
+		});
+	}
+	validateExpression(){
+		try{
+			this.expressionTree = Math.parse(this.expression);
+			console.log('ok');
+			return true;
+		}catch(error){
+			console.log('erro');
+			return false;
+		}
+	}
 };
