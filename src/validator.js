802e70fe90e74164272bf0d3f78c8099fa69f2e5
NedLandy <matheus.v4200@gmail.com>
2020-10-17 20:35:04 -0300

    novos tipos de validações

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,6 @@
 module.exports = class Validator {
 	static isFilled(value) {
+		console.log(value)
 		if (value === '' || value === undefined) {
 			return 'Esse campo é obrigatório';
 		} else {
@@ -23,5 +24,32 @@ module.exports = class Validator {
 				return 'Deve ser um número entre ' + min + ' ' + max;
 			}
 		};
+	}
+
+	static extension(requiredExtension) {
+		return function (value) {
+			if (String(value).endsWith(requiredExtension)) {
+				return true;
+			} else {
+				return 'O arquivo deve ser do tipo ' + requiredExtension;
+			}
+		};
+	}
+
+	static isPositive(value) {
+		if (value >= 0 ) {
+			return true;
+		} else {
+			return 'Deve ser um número maior que 0';
+		}
+	}
+
+	static isNumber(value) {
+		const format = /^[0-9]*$/;
+		if (!format.test(value)) {
+			return 'Não são permitidos caracteres especiais ou letras';
+		} else {
+			return true;
+		}
 	}
 };
