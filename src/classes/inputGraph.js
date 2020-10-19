109d42dfe0239ca5c84b26d8ed8467cc7bc84511
NedLandy <matheus.v4200@gmail.com>
2020-10-19 01:20:22 -0300

    adicionando nos para encapsular os inputs

--- src/classes/inputGraph.js
+++ src/classes/inputGraph.js
@@ -1,20 +1,32 @@
+class Node {
+	constructor(input) {
+		this.name = input.name
+		this.input = input;
+		this.nextInputs = [];
+		this.visited = -1;
+		this.dependencies = input.dependencies;
+	}
+}
 module.exports = class InputGraph {
 	constructor() {
 		this.nodes = [];
+		this.inputs = [];
 		this.nodesDictionary = {};
 	}
 	/**
 	 *  @param {Input} input
 	 */
 	addInput(input) {
-		this.nodes.push(input);
-		this.nodesDictionary[input.name] = input;
-		input.visited = -1;
+		let newNode = new Node(input);
+		this.nodes.push(newNode);
+		this.inputs.push(input);
+		this.nodesDictionary[input.name] = newNode;
+		newNode.visited = -1;
 	}
 
 	resetBusca() {
-		this.nodes.forEach((input) => {
-			input.visited = -1;
+		this.nodes.forEach((node) => {
+			node.visited = -1;
 		})
 	}
 
@@ -22,40 +34,39 @@ module.exports = class InputGraph {
 
 	}
 	/**
-	 *  @param {Input} input
+	 *  @param {Node} input
 	 */
-	recursion(input, currentPos) {
-		if (input.visited === 0) {
+	recursion(node, currentPos) {
+		if (node.visited === 0) {
 			throw new Error('Ciclo Encontrado');
 		}
-		
-		input.visited = 0
-		if (input.level === undefined || input.level < currentPos) {
-			input.level = currentPos;
-			console.log('testando2: ',input)
-			input.nextInputs.forEach((nextInput) => {
+
+		node.visited = 0
+		if (node.level === undefined || node.level < currentPos) {
+			node.level = currentPos;
+			node.nextInputs.forEach((nextInput) => {
 				this.recursion(nextInput, currentPos + 1);
 			})
 		}
 
-		input.visited = 1;
+		node.visited = 1;
 		return true;
 	}
 
 	/**
-	 *  @param {Input} input
+	 *  
 	 */
 
 	addEgdes() {
 
-		this.nodes.forEach(input => {
-			input.nextInputs = [];
-			input.level = undefined;
+		this.nodes.forEach(node => {
+			node.nextInputs = [];
+			node.level = undefined;
 		})
 
-		this.nodes.forEach(input => {
-			input.dependencies.forEach((dependenceName) => {
-				this.nodesDictionary[dependenceName].nextInputs.push(input);
+		this.nodes.forEach(node => {
+			node.dependencies.forEach((dependenceName) => {
+				this.nodesDictionary[dependenceName].nextInputs.push(node);
 			})
 		})
 	}
@@ -63,12 +74,11 @@ module.exports = class InputGraph {
 	hasCycle() {
 
 		this.nodes.forEach(
-			(input) => {
-				if (input.dependencies.length === 0) {
-					console.log('testando: '+ input.name)
+			(node) => {
+				if (node.dependencies.length === 0) {
 					this.resetBusca();
 					try {
-						if (!this.recursion(input, 0)) {
+						if (!this.recursion(node, 0)) {
 							return true;
 						}
 					} catch (error) {
@@ -80,8 +90,10 @@ module.exports = class InputGraph {
 	}
 
 	topologicalSort() {
-		this.nodes.sort((input1, input2) => {
-			return input1.level - input2.level
+		this.inputs.sort((input1, input2) => {
+			return this.nodesDictionary[input1.name].level - this.nodesDictionary[input2.name].level
 		})
 	}
+
+
 }
