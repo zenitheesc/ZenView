86fa32206d4bc6084ad6dc34f6e8129fd7fc107d
Matheus Vieira <matheus.v4200@gmail.com>
2020-10-28 18:39:05 -0300

    início do menu de edição

--- src/components/sideMenu/menus/EditMenu/EditMenu.js
+++ src/components/sideMenu/menus/EditMenu/EditMenu.js
@@ -1,10 +1,56 @@
 const Menu = require('../menu');
+const Form = require('../../../formBuilder/formBuilder').Form;
+const Container = require('../../../formBuilder/formBuilder').Container;
+const Field = require('../../../formBuilder/formBuilder').Field;
+const PlotlyEditingMenuConfig = require('./menus/EditMenus').Plotly;
 
 module.exports = class EditMenu extends Menu {
 
 	constructor() {
 
 		super('Edição', 'edit_menu');
+
+		this.form = new Form({
+			BlockModuleContainer: Container.spliter({
+				module: Field.select({
+					label: 'Selecione um módulo',
+					att: 'module',
+					id: 'BlockModule',
+					options: [{
+						text: 'Plotly',
+					},
+					{
+						text: 'TODO Three.js',
+					},
+					],
+				}),
+			}, {
+				startOpen: true,
+				text: 'Módulos',
+				id: 'BlockModuleContainer',
+			}),
+			PlotlyModule: PlotlyEditingMenuConfig,
+			ThreejsModule: Container.div({
+
+			}, {
+				id: 'ThreejsModule',
+			}),
+			GPSModule: Container.div({
+
+			}, {
+				id: 'GPSModule',
+			}),
+		});
+
+	}
+
+	load() {
+
+		const spliterContainer = document.createElement('div');
+		spliterContainer.className = 'menuBody';
+		spliterContainer.style.height = '100%';
+		spliterContainer.appendChild(this.form.htmlComponent);
+		this.menuComponent.appendChild(spliterContainer);
 
 	}
 
