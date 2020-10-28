89d7bc9aa62a173f4bbdb02b78436dbd20cd92d4
Matheus Vieira <matheus.v4200@gmail.com>
2020-10-28 18:41:00 -0300

    bloco do scatter plot

---
+++ src/components/blocks/Blocks.js
@@ -0,0 +1,4 @@
+const Blocks = {};
+Blocks['2DPlotly'] = require('./Blocks/Plotly2D');
+
+module.exports = Blocks;
---
+++ src/components/blocks/Blocks/Plotly2D.js
@@ -0,0 +1,11 @@
+const Block = require('./block');
+
+module.exports = class Plotly2D extends Block {
+
+	constructor() {
+
+		super();
+
+	}
+
+};
