4a75d815c4047d455b500f1a3f324eb23922a96f
mairacanal <maira.canal@usp.br>
2020-12-20 10:24:47 -0300

    Adequando o arquivo ao padrao de desenvolvimento

--- src/components/titleBar/titleBarMenu.js
+++ src/components/titleBar/titleBarMenu.js
@@ -1,21 +1,25 @@
-const Menu = require("electron").Menu;
+const Menu = require('electron').Menu;
 
 module.exports = class TitleBarMenu {
-    constructor(){
-        const template = [
-            {
-                label: 'Edit',
-                submenu: [
-                  {
-                    role: 'copy'
-                  },
-                  {
-                    role: 'paste'
-                  },
-                ]
-            }];
 
-        this.menu = Menu.buildFromTemplate(template);
-		Menu.setApplicationMenu(this.menu);
-    }
-}
+	constructor() {
+
+		const template = [
+			{
+				label: 'Edit',
+				submenu: [
+					{
+						role: 'copy',
+					},
+					{
+						role: 'paste',
+					},
+				],
+			}];
+
+		this.menu = Menu.buildFromTemplate(template);
+		    Menu.setApplicationMenu(this.menu);
+
+	}
+
+};
