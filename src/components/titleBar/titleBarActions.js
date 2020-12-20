3dbe0eee8f707f2fa50b248f690404c08100f325
mairacanal <maira.canal@usp.br>
2020-12-20 10:16:50 -0300

    Adequando o arquivo ao padrao de desenvolvimento

--- src/components/titleBar/titleBarActions.js
+++ src/components/titleBar/titleBarActions.js
@@ -2,53 +2,72 @@ const ipcRenderer = require("electron").ipcRenderer;
 const remote = require('electron').remote;
 
 module.exports = class TitleBarActions {
+
     constructor(x, y){
+
         this.x = x;
         this.y = y;
         this.browserWindow = remote.getCurrentWindow();
+
     }
+
     openMenu() {
 
         ipcRenderer.send(`display-app-titleBar`, { x: this.x, y: this.y });
 
     }
+
     minimizeWindow() {
 
         if (this.browserWindow.minimizable) {
+
           this.browserWindow.minimize();
+
         }
 
     }
+
     maximizeWindow() {
 
         if (this.browserWindow.maximizable) {
+
           this.browserWindow.maximize();
+
         }
 
     }
+
     unmaximizeWindow() {
 
         this.browserWindow.unmaximize();
 
     }
+
     maxUnmaxWindow() {
 
         if (this.browserWindow.isMaximized()) {
+
             this.browserWindow.unmaximize();
+
         } else {
+
             this.browserWindow.maximize();
+
         }
 
     }
+
     closeWindow() {
 
         console.log('Fechar');
         this.browserWindow.close();
 
     }
+
     isWindowMaximized() {
 
         return this.browserWindow.isMaximized();
 
     }
+    
 }
