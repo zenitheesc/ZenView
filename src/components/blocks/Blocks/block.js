1af1c31dfdf66b1c8462374abfe003d2f1e37882
Matheus Vieira <matheus.v4200@gmail.com>
2021-01-28 18:11:49 -0300

    funcoes basicas para inserir e remover dependencias de um bloco

--- src/components/blocks/Blocks/block.js
+++ src/components/blocks/Blocks/block.js
@@ -4,6 +4,7 @@ module.exports = class Block {
 
 		this.htmlComponent = document.createElement('div');
 		this.htmlComponent.classList.add('grid-stack-item-content');
+		this.inputDependencies = [];
 
 	}
 
@@ -16,6 +17,58 @@ module.exports = class Block {
 	}
 
 	updateConfig() {
+
+	}
+
+	instructionHandler() {
+
+	}
+
+	addDependencie(dependenciesName) {
+
+		if (Array.isArray(dependenciesName)) {
+
+			dependenciesName.forEach((dependencieName) => {
+
+				this.inputDependencies.push(dependencieName);
+
+			});
+
+		} else {
+
+			this.inputDependencies.push(dependencieName);
+
+		}
+
+	}
+
+	rmvDependencie(dependenciesName) {
+
+		if (Array.isArray(dependenciesName)) {
+
+			dependenciesName.forEach((dependencieName) => {
+
+				const index = this.inputDependencies.indexOf(dependencieName);
+
+				if (index > -1) {
+
+					array.splice(index, 1);
+
+				}
+
+			});
+
+		} else {
+
+			const index = this.inputDependencies.indexOf(dependenciesName);
+
+			if (index > -1) {
+
+				array.splice(index, 1);
+
+			}
+
+		}
 
 	}
 
