296d560eac31bbc2b90e1a984a58080a38a9442c
Matheus Vieira <matheus.v4200@gmail.com>
2021-03-11 16:21:52 -0300

    importando bloco do uPlot

--- src/components/blocks/Blocks.js
+++ src/components/blocks/Blocks.js
@@ -1,5 +1,6 @@
 const Blocks = {};
 Blocks['Plotly'] = require('./Blocks/plotly/Plotly');
+Blocks['uPlot'] = require('./Blocks/uPlot/uPlot');
 Blocks['Blank'] = require('./Blocks/blank/blank');
 
 module.exports = Blocks;
