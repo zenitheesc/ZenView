9e461cb52c55f7432f36fb8564d640bc5d92b2c3
Matheus Vieira <matheus.v4200@gmail.com>
2021-03-10 22:36:26 -0300

    menu de plots do uplot

---
+++ src/components/sideMenu/menus/EditMenu/menus/uPlot/uPlot.js
@@ -0,0 +1,61 @@
+const Container = require('../../../../../formBuilder/formBuilder').Container;
+const Field = require('../../../../../formBuilder/formBuilder').Field;
+const uPlotScatter = require('./types/scatter');
+
+
+module.exports = class uPlotEditMenu {
+
+	constructor() {
+
+		this.uPlotScatter = new uPlotScatter();
+		
+		this.form = Container.div({
+			plotTypes: Container.spliter(
+				{
+					plotType: Field.select({
+						label: 'Selecione um tipo: ',
+						att: 'type',
+						id: 'uPlotTypeSelector',
+						options: [
+							{
+								text: 'scatter',
+							},
+							{
+								text: 'bar',
+							},
+							{
+								text: 'heatmap',
+							},
+							{
+								text: 'time serie',
+							},
+							{
+								text: 'sparkline',
+							},
+						],
+					}),
+				},
+				{
+					startOpen: true,
+					text: 'Tipos de plot',
+					id: 'uPlotType',
+				},
+			),
+			scatterConfig: this.uPlotScatter.form,
+		},
+			{
+				id: 'uPlotEditMenuConfig',
+				att: 'uPlot',
+				conditions: [
+					{
+						id: 'BlockModule',
+						att: 'value',
+						requiredValue: 'uPlot',
+					},
+				],
+			},
+		);
+
+	}
+
+};
