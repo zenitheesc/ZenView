3727a9038496e1c9c9e4191ab522e1a47219c4ac
Arthur Grillo <arthur.grillo@usp.br>
2021-09-09 13:09:07 -0300

    Started the info page

--- src/components/sideMenu/menus/InfoMenu/InfoMenu.js
+++ src/components/sideMenu/menus/InfoMenu/InfoMenu.js
@@ -8,4 +8,25 @@ module.exports = class InfoMenu extends Menu {
 
 	}
 
+    progResum(){
+        const resume = document.createElement('div');
+        resume.textContent = 'ZenView posibilita a criação de plots 2D e GPS para análise de dados adquiridos pela sonda';
+        resume.style.color = 'white';
+        this.menuComponent.appendChild(resume);
+    }
+
+    progInfo(){
+        const info = document.createElement('div');
+        info.style.textAlign = 'center';
+        const version = document.createElement('h5');
+        version.textContent = 'v1.0';
+        info.appendChild(version);
+        this.menuComponent.appendChild(info);
+    }
+
+    load(){
+        this.progResum();
+        this.progInfo();
+    }
+
 };
