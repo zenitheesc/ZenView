ae32bd93c01b68123975348efcf5a5f19b2da0c1
angelobguido <angelobguido@gmail.com>
2022-02-08 11:43:24 -0300

    feature/info: Add open external link property

--- src/components/sideMenu/menus/InfoMenu/InfoMenu.js
+++ src/components/sideMenu/menus/InfoMenu/InfoMenu.js
@@ -16,11 +16,32 @@ module.exports = class InfoMenu extends Menu {
     }
 
     progInfo(){
+        const shell = require('electron').shell;
+        
         const info = document.createElement('div');
-        info.style.textAlign = 'center';
+        info.style.color = 'white';
+        
         const version = document.createElement('h5');
         version.textContent = 'v1.0';
+        version.style.textAlign = 'center';
+        
+        const aboutUs = document.createElement('h4');
+        aboutUs.textContent ='Sobre nós:'
+        
+        const aboutUsText = document.createElement('p');
+        aboutUsText.textContent = 'O Zenith é uma extracurricular da USP, nosso foco é no desenvolvimento de sondas e satélites. Dessa forma, o grupo se ramifica em diversas áreas da tecnologia, como hardware, software, estrutura e entre outras. ';
+        
+        const zenithLink = document.createElement('p');
+        zenithLink.textContent = 'Visite o nosso site';
+        zenithLink.addEventListener('click', event=>shell.openExternal('https://zenith.eesc.usp.br/'));
+        zenithLink.style.color='blue';
+        zenithLink.style.cursor='pointer';
+        
+        info.appendChild(aboutUs);
+        info.appendChild(aboutUsText);
         info.appendChild(version);
+        
+        aboutUsText.appendChild(zenithLink);
         this.menuComponent.appendChild(info);
     }
 
